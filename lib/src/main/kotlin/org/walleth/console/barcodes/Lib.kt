f4d455ecd489aa6e7e5707a30cac8b17771f03db
ligi <ligi@ligi.de>
2018-10-08 20:21:55 +0200

    Add option to print small and large QR codes

--- lib/src/main/kotlin/org/walleth/console/barcodes/Lib.kt
+++ lib/src/main/kotlin/org/walleth/console/barcodes/Lib.kt
@@ -1,29 +1,47 @@
 package org.walleth.console.barcodes
 
 import com.google.zxing.BarcodeFormat.QR_CODE
+import com.google.zxing.common.BitMatrix
 import com.google.zxing.qrcode.QRCodeWriter
 
-private const val WHITE_ALL = '\u2588'
-private const val WHITE_BLACK = '\u2580'
-private const val BLACK_WHITE = '\u2584'
-private const val BLACK_ALL = ' '
-
-fun printQR(content: String) {
+fun printQR(content: String, large: Boolean = false) {
 
     val writer = QRCodeWriter().encode(content, QR_CODE, 0, 0)
 
-    for (y in 0 until writer.height step 2) {
-        println((0 until writer.width).map { x ->
-            val nextRowPixel = (y + 1 < writer.height) && writer.get(x, y + 1)
-            if (writer.get(x, y)) {
-                if (nextRowPixel) BLACK_ALL else BLACK_WHITE
-            } else {
-                if (nextRowPixel) WHITE_BLACK else WHITE_ALL
-            }
-        }.joinToString(""))
+    if (large) {
+        writer.printLarge()
+    } else {
+        writer.printSmall()
+    }
+}
+
+private const val WHITE_2x2 = "\u2588\u2588"
+private const val BLACK_2x2 = "  "
+
+fun BitMatrix.printLarge() {
+
+    for (y in 0 until height) {
+        println((0 until width).joinToString("") { x ->
+            if (get(x, y)) BLACK_2x2 else WHITE_2x2
+        })
     }
 
 }
 
+private const val WHITE_ALL_1x1 = '\u2588'
+private const val WHITE_BLACK_1x1 = '\u2580'
+private const val BLACK_WHITE_1x1 = '\u2584'
+private const val BLACK_ALL_1x1 = ' '
 
-
+private fun BitMatrix.printSmall() {
+    for (y in 0 until height step 2) {
+        println((0 until width).map { x ->
+            val nextRowPixel = (y + 1 < height) && get(x, y + 1)
+            if (get(x, y)) {
+                if (nextRowPixel) BLACK_ALL_1x1 else BLACK_WHITE_1x1
+            } else {
+                if (nextRowPixel) WHITE_BLACK_1x1 else WHITE_ALL_1x1
+            }
+        }.joinToString(""))
+    }
+}
